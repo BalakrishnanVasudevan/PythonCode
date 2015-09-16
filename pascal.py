09ce51c20fa4ea894e714c504739da6895727e80
Bala Vasudevan <bala@Balas-MacBook-Pro.local>
2015-09-16 11:05:15 -0700

    Function Added

--- pascal.py
+++ pascal.py
@@ -1,11 +1,19 @@
-rows = 5
-for rownum in range (rows):
-    newValue=1
-    print 'rownum=', rownum
-    PrintingList = [newValue]
-    for iteration in range (rownum):
-    	print 'iteration', iteration
-        newValue = newValue * ( rownum-iteration ) * 1 / ( iteration + 1 )
-        PrintingList.append(int(newValue))
-        print 'newValue=', newValue
-    print(PrintingList)
+import math
+def pascal(rows):
+ for rownum in range (rows):
+  newValue=1
+  PrintingList = [newValue]
+  for iteration in range (rownum):
+   newValue = newValue * ( rownum-iteration ) * 1 / ( iteration + 1 )
+   PrintingList.append(int(newValue))
+  print(PrintingList)
+
+
+
+rows = input('Enter the number of rows \n')
+pascal(rows)
+print 'enter the row and column of the element to be looked up \n'
+a = input('enter the row: \n')
+b = input('enter the column: \n')
+print 'Element \n'
+print math.factorial(a)/((math.factorial(a-b))*math.factorial(b))
