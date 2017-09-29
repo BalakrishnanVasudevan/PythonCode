678fdaa32886dcd17bffc3c1baeb9b4c0c5a3da1
Balakrishnan Vasudevan <bala1990krishna@gmail.com>
2017-09-28 20:21:24 -0700

    bala: late commit

---
+++ swap2numbers.py
@@ -0,0 +1,7 @@
+num1 = input('Enter the first number: ')
+num2 = input('Enter the second number: ')
+print("num1=",num1,"num2=",num2)
+num1 = num1+num2
+num2 = num1 - num2
+num1 = num1 - num2
+print("num1=",num1,"num2=",num2)
